b16ca162a7bebdbe3184f05af1cef23554d00335
shivsharma <chhail.bihari_mca18@gla.ac.in>
2019-01-17 11:34:16 +0530

    add toast in action listner

--- app/src/main/java/sharma/shiv/mycalculator/MainActivity.java
+++ app/src/main/java/sharma/shiv/mycalculator/MainActivity.java
@@ -2,12 +2,31 @@ package sharma.shiv.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "hello alok pandey", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
